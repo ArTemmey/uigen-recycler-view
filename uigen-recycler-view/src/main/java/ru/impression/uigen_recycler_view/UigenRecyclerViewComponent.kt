3649252998911c9e739d1151c29fb41c7f1fdb66
Lunkov_A@utkonos.ru <Aezakmihesoyam321>
2020-12-07 14:01:59 +0300

    rebuilt component

--- uigen-recycler-view/src/main/java/ru/impression/uigen_recycler_view/UigenRecyclerViewComponent.kt
+++ uigen-recycler-view/src/main/java/ru/impression/uigen_recycler_view/UigenRecyclerViewComponent.kt
@@ -16,76 +16,78 @@ import kotlin.jvm.JvmStatic
 import ru.impression.ui_generator_base.Component
 import ru.impression.ui_generator_base.ComponentViewModel
 import ru.impression.ui_generator_base.Renderer
-import ru.impression.ui_generator_base.set
+import ru.impression.ui_generator_base.resolveAttrs
+import ru.impression.ui_generator_base.safeSetProp
 
 /*
  This is a copy of generated class. There is some problem using generated views from third party
  libraries.
  */
 class UigenRecyclerViewComponent @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+  context: Context,
+  attrs: AttributeSet? = null,
+  defStyleAttr: Int = 0
 ) : RecyclerView(context, attrs, defStyleAttr), Component<RecyclerView, UigenRecyclerViewModel>,
     LifecycleOwner {
-    override val scheme: UigenRecyclerView = UigenRecyclerView()
+  override val scheme: UigenRecyclerView = UigenRecyclerView()
 
-    override val viewModel: UigenRecyclerViewModel =
-        createViewModel(ru.impression.uigen_recycler_view.UigenRecyclerViewModel::class)
+  override val viewModel: UigenRecyclerViewModel =
+      createViewModel(ru.impression.uigen_recycler_view.UigenRecyclerViewModel::class)
 
-    override val container: View = this
+  override val container: View = this
 
-    override val boundLifecycleOwner: LifecycleOwner = this
+  override val boundLifecycleOwner: LifecycleOwner = this
 
-    override val renderer: Renderer = Renderer(this)
+  override val renderer: Renderer = Renderer(this)
 
-    private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
+  private val lifecycleRegistry: LifecycleRegistry = LifecycleRegistry(this)
 
-    private var isDetachedFromWindow: Boolean = false
+  private var isDetachedFromWindow: Boolean = false
 
-    init {
+  init {
+    resolveAttrs(attrs)
         render(false)
         startObservations()
+  }
+
+  override fun getLifecycle() = lifecycleRegistry
+  override fun onAttachedToWindow() {
+    super.onAttachedToWindow()
+    lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    if (isDetachedFromWindow) {
+      isDetachedFromWindow = false
+      startObservations()
+    }
+  }
+
+  override fun onDetachedFromWindow() {
+    super.onDetachedFromWindow()
+    lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    viewModel.onCleared()
+    isDetachedFromWindow = true
+  }
+
+  companion object {
+    @JvmStatic
+    @BindingAdapter("getItemBindingClass")
+    fun setGetItemBindingClass(view: UigenRecyclerViewComponent,
+        value: UigenRecyclerViewModel.GetItemBindingClass?) {
+      if (value === view.viewModel.getItemBindingClass) return
+      view.viewModel::getItemBindingClass.safeSetProp(value)
     }
 
-    override fun getLifecycle() = lifecycleRegistry
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        if (isDetachedFromWindow) {
-            isDetachedFromWindow = false
-            startObservations()
-        }
+    @JvmStatic
+    @BindingAdapter("data")
+    fun setData(view: UigenRecyclerViewComponent, value: List<*>?) {
+      if (value === view.viewModel.data) return
+      view.viewModel::data.safeSetProp(value)
     }
 
-    override fun onDetachedFromWindow() {
-        super.onDetachedFromWindow()
-        lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
-        viewModel.onCleared()
-        isDetachedFromWindow = true
+    @JvmStatic
+    @BindingAdapter("viewModel")
+    fun setViewModel(view: UigenRecyclerViewComponent, value: ComponentViewModel?) {
+      if (value === view.viewModel.viewModel) return
+      view.viewModel::viewModel.safeSetProp(value)
     }
-
-    companion object {
-        @JvmStatic
-        @BindingAdapter("getItemBindingClass")
-        fun setGetItemBindingClass(view: UigenRecyclerViewComponent,
-                                   value: UigenRecyclerViewModel.GetItemBindingClass?) {
-            if (value === view.viewModel.getItemBindingClass) return
-            view.viewModel::getItemBindingClass.set(view.viewModel, value)
-        }
-
-        @JvmStatic
-        @BindingAdapter("data")
-        fun setData(view: UigenRecyclerViewComponent, value: List<*>?) {
-            if (value === view.viewModel.data) return
-            view.viewModel::data.set(view.viewModel, value)
-        }
-
-        @JvmStatic
-        @BindingAdapter("viewModel")
-        fun setViewModel(view: UigenRecyclerViewComponent, value: ComponentViewModel?) {
-            if (value === view.viewModel.viewModel) return
-            view.viewModel::viewModel.set(view.viewModel, value)
-        }
-    }
+  }
 }
